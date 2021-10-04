1886e55eb337853c99e74558116b93c9530ed1bd
“Hassan <mian@willandskill.se>
2021-10-04 14:07:02 +0200

    Refactor

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
         const purchase_history = [];
         // Defines an array populated with objects/dictionaries/hashmaps
         const coffees = [
-            { name: 'Brygg Kaffe', price: 20 },
-            { name: 'Cappucino', price: 30 },
-            { name: 'Latte', price: 40 }
+            { name: 'Brygg Kaffe', price: 10, selected: false },
+            { name: 'Latte', price: 45, selected: true },
+            { name: 'Cappucino', price: 25, selected: false }
         ]
 
         class Customer {
@@ -14,22 +14,76 @@
             appendTransaction(transaction) {
                 let parent = document.querySelector('ul')
                 let child = document.createElement('li');
-                child.innerHTML = `Du köpte ${transaction.amount} ${transaction.coffee.name} för ${transaction.coffee.price} styck. Summa: ${transaction.coffee.price * transaction.amount} kr`;
+                let message = `
+                    Du har köpt ${transaction.coffee.name} för ${transaction.price} styck. Totalt värde: ${transaction.total} kr.
+                    Du var ${transaction.membership} när denna transaktion utfördes. 
+                    
+                `;
+                if(transaction.membership !== "Ultra Premium") {
+                    message += `Handla ${transaction.cupsUntilNextLevel} koppar till för att upp nå ${this.getNextLevel(transaction.membership)} status`
+                }
+                child.innerHTML = message
                 // prepend() method will put the new child element before the first child of the element.
                 parent.prepend(child);
             }
 
             decideLevel(spent) {
-                if (spent < 10) return 'Brons';
-                if (spent >= 10 && spent < 30) return 'Silver';
-                if (spent >= 30) return 'Guld';
+                if (spent < 5) return 'Normal';
+                if (spent >= 5 && spent < 15) return 'Premium';
+                if (spent >= 15) return 'Ultra Premium';
+            }
+            
+            cupsUntilNextLevel() {
+                const totalCups = this.getTotalCupPurchased()
+                if(totalCups < 5) {
+                    return 5 - totalCups
+                } else if(5 <= totalCups && totalCups < 15) {
+                    return 15 - totalCups
+                } else if (totalCups >= 15) {
+                    return 0
+                }
             }
 
+            getNextLevel(level) {
+                if(level === "Normal") {
+                    return "Premium"
+                } else if(level === "Premium"){
+                    return "Ultra Premium"
+                } else {
+                    return ""
+                }
+            }
+
+            getTotalCupPurchased() {
+                let totalCups = 0
+                for (const history of purchase_history) {
+                    totalCups += history.amount
+                }
+                return totalCups
+
+            }
+
+            getTotalPurchased() {
+                let purchased_amount = 0
+                for (const history of purchase_history) {
+                    purchased_amount += history.total
+                }
+                return purchased_amount
+            }
+
+            getDiscount() {
+                const purchased_amount = this.getTotalPurchased()
+                if(purchased_amount > 800 && purchased_amount < 1200) {
+                    return 0.95
+                } else if(purchased_amount >= 1200) {
+                    return 0.85
+                } else {
+                    return 1
+                }
+            }
             output() {
                 // This variable holds the total amount of cups we've purchased for 
-                let purchased_amount = 0
                 // This variable holds the total amount of money we've purchased for 
-                let total_purchased_kr = 0
 
                 // We get the tag select in for us to use selectedIndex in transactions.
                 // By not doing this we wouldn't be able to access the array with objects.
@@ -52,25 +106,29 @@
                    i.e: if selected index is 0, we get the first object from the coffees array. 
                 */
                 let transaction = {
-                    amount: amount,
-                    coffee: coffees[select.selectedIndex]
+                    amount: parseInt(amount),
+                    coffee: coffees[select.selectedIndex],
+                    
                 };
 
                 // We append the transaction object to our empty purchase_history array.
                 purchase_history.push(transaction);
+                let latest = purchase_history[purchase_history.length-1]
+                
+                latest.price = coffees[select.selectedIndex].price * this.getDiscount(),
+                latest.total = coffees[select.selectedIndex].price * parseInt(amount) * this.getDiscount(),
+                latest.membership = this.decideLevel(this.getTotalCupPurchased()),
+                latest.cupsUntilNextLevel = this.cupsUntilNextLevel()
+                purchase_history[purchase_history.length -1 ] = latest
 
                 /* 
                    for..of is used to calculate each element of the array 
                    It loops through the values of an iterable object
                    previous value + (coffee_price * coffee_amount)
                 */
-                for (const history of purchase_history) {
-                    total_purchased_kr += (history.coffee.price * history.amount)
-                    purchased_amount += history.amount
-                }
 
                 // We loop through our purchase_history array into our id purchased_total 
-                purchased_total.textContent = `${total_purchased_kr} kr`;
+                purchased_total.textContent = `${this.getTotalPurchased()} kr`;
 
                 /* 
                     We append the transaction object, this is what makes the purchase text show up.
@@ -80,7 +138,7 @@
                 this.appendTransaction(transaction);
 
                 // We calculate the level by adding previous value + coffee amount
-                let level = this.decideLevel(purchased_amount);
+                let level = this.decideLevel(this.getTotalCupPurchased());
 
                 document.getElementById('membership-status').textContent = level;
                 // This changes the textcontent from "Du har inga transaktioner" to "Dina transaktioner"
@@ -95,7 +153,7 @@
             const coffeeList = document.getElementById("coffeeList")
             coffees.forEach((coffee, index) => {
                 coffeeList.options.add(
-                    new Option(`${coffee.name} - ${coffee.price}`, index)
+                    new Option(`${coffee.name} - ${coffee.price}`, index, coffee.selected, coffee.selected)
                 )
             })
             document.querySelector('button').addEventListener('click', () => {
